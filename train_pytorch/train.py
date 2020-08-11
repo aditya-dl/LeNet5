dc7558b1ed07dd06d902301a1fc8e0594615d2d6
aditya-dl <adityaslohia29@gmail.com>
2020-08-11 18:28:18 +0530

    update print statement to 100 mini batches

--- train_pytorch/train.py
+++ train_pytorch/train.py
@@ -41,7 +41,7 @@ for epoch in range(epochs):
 
         # print statistics
         running_loss += loss.item()
-        if i%2000 == 1999:
+        if i % 100 == 99:
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
